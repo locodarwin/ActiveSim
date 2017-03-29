ee0c4cbaaa3ff5196eb46a408f00dc504360e965
Locodarwin <locodarwin@yahoo.com>
2017-03-28 17:02:23 -0700

    3/28/17

---
+++ ActiveSim/Misc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveSim
+{
+    public partial class Form1
+    {
+        private bool CheckPerms(int iCitnum, string sCommand)
+        {
+
+            return false;
+        }
+    }
+}
